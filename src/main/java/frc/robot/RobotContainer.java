f41862ec0ec70c369ecea8cd15101801f2043ec3
EPA Robotics Laptop <55808130+epa-robotics-team@users.noreply.github.com>
2024-11-08 22:41:35 -0800

    added super simple xbox control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,42 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
 
+  static final class Constants {
+    public static final int kDriverControllerPort = 0;
+    public static final double kDriveDeadband = 0.1;
+  }
+
+  final XboxController driverController = new XboxController(Constants.kDriverControllerPort);
+
+  Drivetrain drivetrain = new Drivetrain();
+
+  final Command fastDrive = new RunCommand(
+      () -> drivetrain.drive(
+          -MathUtil.applyDeadband(driverController.getLeftY(),
+              Constants.kDriveDeadband),
+          -MathUtil.applyDeadband(driverController.getLeftX(),
+              Constants.kDriveDeadband),
+          -MathUtil.applyDeadband(driverController.getRightX(),
+              Constants.kDriveDeadband),
+          true, true),
+      drivetrain);
+
   public RobotContainer() {
+    drivetrain.setDefaultCommand(fastDrive);
   }
 
   public Command getAutonomousCommand() {
     var doNothing = new InstantCommand();
-       return doNothing; 
+    return doNothing;
   }
 
 }
